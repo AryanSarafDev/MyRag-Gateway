ccbf622fdb8ea35db8a55d192d2c7ef56642e88a
AryanSarafDev <aryansarafdev@gmail.com>
2026-05-22 18:32:32 +0530

    Auth filter

--- src/main/java/com/myrag/gateway/Service/JwtService.java
+++ src/main/java/com/myrag/gateway/Service/JwtService.java
@@ -1,5 +1,52 @@
 package com.myrag.gateway.Service;
 
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Service
 public class JwtService {
+
+    @Value("${app.jwt.secret}")
+    private String secret;
+
+    @Value("${app.jwt.expiration}")
+    private long expiration;
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String generateToken(String email) {
+        return Jwts.builder().
+        subject(email).
+        issuedAt(new Date()).
+        expiration(new Date(System.currentTimeMillis() + expiration)).
+        signWith(getSigningKey()).
+        compact();
+    }
+    public String extractEmail(String token){
+        return extractClaims(token).getSubject(); 
+    }
+
+    public boolean isTokenValid(String token) {
+       try{
+        Claims claim = extractClaims(token);
+        return !claim.getExpiration().after(new Date());
+       }catch(Exception e){
+        return false;
+       }
+    }
+
+    private Claims extractClaims(String token){
+        return Jwts.parser().verifyWith(getSigningKey()).build().parseSignedClaims(token).getPayload();
+    }
 
 }
